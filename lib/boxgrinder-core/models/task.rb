e5c73457275fbb5647b57237e527b0434ee93474
Marek Goldmann <marek.goldmann@gmail.com>
2010-03-12 12:57:56 +0100

    removed artifact from task

--- lib/boxgrinder-core/models/task.rb
+++ lib/boxgrinder-core/models/task.rb
@@ -20,15 +20,13 @@
 
 module BoxGrinder
   class Task
-    def initialize( artifact, action, description, data = {} )
+    def initialize( action, description, data = {} )
       @created_at       = Time.now
-      @artifact         = artifact
       @action           = action
       @description      = description
       @data             = data
     end
 
-    attr_reader :artifact
     attr_reader :action
     attr_reader :description
     attr_reader :created_at
