b49a480f8760429a823d857e2e37a4090dab1048
Marek Goldmann <marek.goldmann@gmail.com>
2011-07-13 10:34:54 +0200

    rspec requires

--- spec/helpers/appliance-config-helper-spec.rb
+++ spec/helpers/appliance-config-helper-spec.rb
@@ -17,6 +17,7 @@
 # 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 
 require 'rubygems'
+require 'rspec'
 require 'boxgrinder-core/helpers/appliance-config-helper'
 require 'boxgrinder-core/models/appliance-config'
 
