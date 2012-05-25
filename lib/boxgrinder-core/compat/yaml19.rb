b98a89b99924acf743c203449dfe33ec8504232f
Marc Savy <msavy@redhat.com>
2012-05-25 15:24:08 +0100

    Improve yaml19 patch

--- lib/boxgrinder-core/compat/yaml19.rb
+++ lib/boxgrinder-core/compat/yaml19.rb
@@ -1,6 +1,6 @@
 module BoxGrinder
   # Avoid Psych::SyntaxError (<unknown>): couldn't parse YAML ... in 1.9
-  if RUBY_VERSION.split('.')[1] == '9'
+  if RUBY_VERSION >= '1.9'
     require 'yaml'
     YAML::ENGINE.yamler = 'syck'
   end
